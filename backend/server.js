c3cae4f2d19fc9075e28d99a44266ea1e46e5df3
UmeshDesaboina <149916679+UmeshDesaboina@users.noreply.github.com>
2025-10-25 19:27:21 +0530

    Reorganize server.js for improved structure

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 require('dotenv').config();
 const app = require('./app');
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 app.get("/", (req, res) => {
   res.send("Backend is running!");
 });
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
