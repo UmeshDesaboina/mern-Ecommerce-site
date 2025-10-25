29037172bc8e08a699a9cf4b81b8e21306ace6ff
UmeshDesaboina <149916679+UmeshDesaboina@users.noreply.github.com>
2025-10-25 19:11:03 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,3 +3,6 @@ const app = require('./app');
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.get("/", (req, res) => {
+  res.send("Backend is running!");
+});
